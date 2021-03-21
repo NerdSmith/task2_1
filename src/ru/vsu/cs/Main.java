aabeabb6d44d1cc2e715ebe794824afc6c5da019
Nikita Merzlyakov <merzlyakov02@gmail.com>
2021-03-21 23:53:19 +0300

    added all commands

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -1,14 +1,9 @@
 package ru.vsu.cs;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
         FileSystem fs = new FileSystem("mainDir");
         fs.start();
-
-
-
     }
 }
